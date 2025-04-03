0d2198efa59f7328673d5c337d7fb9b458e7cde8
HAO-317 <lihao317@gmail.com>
2025-04-03 23:57:12 +0200

    test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,7 +11,7 @@ const About: React.FC = () => {
         <div className="main-content">
           <header className="headline">
             <h1 className="text-wrapper-4">Who we are</h1>
-            <img className="group-3" src='/img/group28.png' alt="Decorative icon" />
+            <img className="group-3" src='${process.env.PUBLIC_URL}/img/group28.png' alt="Decorative icon" />
             <p className="text-wrapper-5">ABOUT US</p>
             <hr className="line-2" />
           </header>
@@ -23,7 +23,7 @@ const About: React.FC = () => {
             </h2>
             <p className="p">the answer to your need for furniture with shapes, sizes and colors.</p>
           </section>
-          <img className="group-2" src='/img/About.png' alt="Furniture showcase" />
+          <img className="group-2" src='${process.env.PUBLIC_URL}/img/About.png' alt="Furniture showcase" />
         </div>
       </main>
       <Footer />
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -12,24 +12,24 @@ const Products: React.FC = () => {
         <section className="product-section">
           <div className="headline">
             <h1 className="text-wrapper-4">Who we are</h1>
-            <img className="group-3" src='/img/group28.png' alt="Decorative icon" />
+            <img className="group-3" src='${process.env.PUBLIC_URL}/img/group28.png' alt="Decorative icon" />
             <p className="text-wrapper-5">ABOUT US</p>
             <hr className="line-2" />
           </div>
           <div className="product-list">
             <article className="product-card">
               <h2 className="product-card_title">Jenson</h2>
-              <img className="product-card_image" src='/img/Tisch.png' alt="Jenson product" />
+              <img className="product-card_image" src='${process.env.PUBLIC_URL}/img/Tisch.png' alt="Jenson product" />
               <Link to="/jenson" className="product-card_button">Shop now</Link> 
             </article>
             <article className="product-card">
               <h2 className="product-card_title">Deon</h2>
-              <img className="product-card_image" src='/img/Stuhl.png' alt="Deon product" />
+              <img className="product-card_image" src='${process.env.PUBLIC_URL}/img/Stuhl.png' alt="Deon product" />
               <Link to="/deon" className="product-card_button">Shop now</Link> 
             </article>
             <article className="product-card">
               <h2 className="product-card_title">Krisha</h2>
-              <img className="product-card_image" src='/img/Schuesseln.png' alt="Krisha product" />
+              <img className="product-card_image" src='${process.env.PUBLIC_URL}/img/Schuesseln.png' alt="Krisha product" />
               <Link to="/krisha" className="product-card_button">Shop now</Link> 
             </article>
           </div>
